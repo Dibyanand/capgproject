2b713588bae5dc34957fcae2fa46526564500826
Dibyanand <ddibyana@DIN52003441.corp.capgemini.com>
2016-07-20 15:57:48 +0530

    Test 1

--- Day1_productAPP/src/main/java/com/db/product/ProductMain.java
+++ Day1_productAPP/src/main/java/com/db/product/ProductMain.java
@@ -5,7 +5,7 @@ public class ProductMain {
 	public static void main(String[] args) {
 		System.out.println(" Git Example");
 		
-		System.out.println(" Git Example 1");
+		System.out.println(" Test 1");
 	}
 
 }
